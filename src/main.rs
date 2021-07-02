c289c608bc89a8008043e2a995448731c716f06b
PrestonCazier <prestoncazier@gmail.com>
2021-07-02 16:07:46 -0700

    refactor code to use modules

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use rand::random;
 use std::time::Instant;
+use std::convert:From;
 
+mod ray;
+mod cart_coord;
+mod cylinder_coord;
+mod sphere_coord;
 //use rand::Rng;
 
-struct CartCoord {
-	x: f32,
-	y: f32,
-	z: f32,
-}
-
-struct CylindricalPolarRay {
-	angle: f32,
-	height: f32,
-}
 
 fn main() {
 	let eye_coordinates = CartCoord {
@@ -64,47 +59,123 @@ fn main() {
 	println!("Time per cast: {:?}.", test_time / 100000);
 }
 
-fn cast_to_sphere(
-	eye_coord: &CartCoord,
-	sphere_coord: &CartCoord,
-	sphere_radius: f32,
-	ray_direction: &CylindricalPolarRay,
-) -> Option<f32> {
-	//Convert cylindrical polar coordinates to cartesian.
-	let (ray_y, ray_x) = ray_direction.angle.sin_cos();
-	let ray_z = ray_direction.height;
-
-	//Offset eye coordinates by target location so we can treat the sphere as the origin.
-	let eye_x = eye_coord.x - sphere_coord.x;
-	let eye_y = eye_coord.y - sphere_coord.y;
-	let eye_z = eye_coord.z - sphere_coord.z;
-
-	//Do raycast
-	let a = ray_x.powi(2) + ray_y.powi(2) + ray_z.powi(2);
-	let b = 2.0 * eye_x * ray_x + 2.0 * eye_y * ray_y + 2.0 * eye_z * ray_z;
-	let c = eye_x.powi(2) + eye_y.powi(2) + eye_z.powi(2) - sphere_radius.powi(2);
-	let result_one = (-1.0 * b + (b.powi(2) - 4.0 * a * c).sqrt()) / 2.0 * a;
-	let result_two = (-1.0 * b - (b.powi(2) - 4.0 * a * c).sqrt()) / 2.0 * a;
-
-	if result_one >= 0.0 {
-		if result_two >= 0.0 {
-			if result_one < result_two {
-				return Some(result_one);
-			} else {
-				return Some(result_two);
-			}
-		} else {
-			return Some(result_one);
-		}
-	} else if result_two >= 0.0 {
-		return Some(result_two);
-	} else {
-		return None;
-	}
-}
-
 /*
 fn cast_to_cylinder() {
 
 }
 */
+
+/*
+direct spherical polar (horizontal angle, vertical angle)
+assume the object is a sphere
+field of view is an angle of view singular thus creating a square as this angle is horizontal and vertical
+
+given these 3 draw the object in a grid
+*/
+fn spatial_resolution_scan_spherical(
+	direction:SphericalPolarRay, 
+	object:CartCoord, 
+	fieldofview:SphericalPolarRay,
+	resolution:SpatialResolution) {
+
+
+
+
+	projection_radius = 1.0;
+
+	angle_one = SphericalPolarRay {
+		horz_angle:direction.horz_angle-fieldofview.horz_angle/2,
+		vert_angle:direction.vert_angle
+	};
+	angle_two = SphericalPolarRay {
+		horz_angle:direction.horz_angle,
+		vert_angle:direction.vert_angle-fieldofview.vert_angle/2
+	};
+
+	horz_proj_rad = 
+	vert_proj_rad = 
+	point_horz = CartCoord {
+		x: projection_radius*angle_one.polar.to_radians.sin()*angle_one.azimuthal.cos(),
+		y: projection_radius*angle_one.polar.to_radians.sin(),angle_one.azimuthal.to_radians.sin()
+		z: projection_radius*angle_one.polar.to_radians.cos()
+	};
+	point_vert = CartCoord {
+		x: projection_radius*angle_two.polar.to_radians.sin()*angle_two.azimuthal.cos(),
+		y: projection_radius*angle_two.polar.to_radians.sin(),angle_two.azimuthal.to_radians.sin()
+		z: projection_radius*angle_two.polar.to_radians.cos()
+	};
+	point_center = CartCoord {
+		x: projection_radius*direction.polar.to_radians.sin()*direction.azimuthal.cos(),
+		y: projection_radius*direction.polar.to_radians.sin()*direction.azimuthal.to_radians.sin()
+		z: projection_radius*direction.polar.to_radians.cos()
+	};
+
+	horizontal_direction = CartCoord {
+		x:point_two.x-point_one.x,
+		y:,
+		z:
+	};
+	vertical_direction = CartCoord {
+		x:,
+		y:,
+		z:
+	};
+	/*loop object cast in field of view direct*/
+}
+
+fn angular_resolution_scan_spherical(
+	eye_coord: &CartCoord,
+	direction:&SphericalPolarRay, 
+	object:&CartCoord, 
+	sphere_radius: f32,
+	fieldofview:&SphericalPolarRay,
+	resolution:&SpatialResolution
+) -> Option<f32> {
+	let projection_radius = 1.0;
+	let start_ray = SphericalPolarRay {
+		azimuthal:direction.azimuthal-0.5*fieldofview.azimuthal, 
+		polar:direction.polar-0.5*fieldofview.polar
+	};
+	let h_angle_delta = fieldofview.azimuthal / resolution.width;
+	let v_angle_delta = fieldofview.polar / resolution.height;	
+	for i in 0..resolution.width {
+		for j in 0..resolution.height {
+			//convert to correct coords
+			let fire_point = CartCoord::from(SphereCoord {
+				r:projection_radius,
+				azimuthal:start_ray.azimuthal+i*h_angle_delta,
+				polar:start_ray.polar+j*v_angle_delta
+			}) + eye_coord;
+			//fire ray
+			let result = cast_to_sphere(eye_coord, object, sphere_radius, fire_point);
+			//record result
+		}
+	}
+}
+
+fn angular_resolution_scan_cylindrical(
+	eye_coord: &CartCoord,
+	direction:&SphericalPolarRay, 
+	object:&CartCoord, 
+	sphere_radius: f32,
+	fieldofview:&SphericalPolarRay,
+	resolution:&SpatialResolution
+) -> Option<f32> {
+	projection_radius = 1.0;
+	start_ray = SphericalPolarRay {
+		azimuthal:direction.azimuthal-0.5*fieldofview.azimuthal, 
+		polar:direction.polar-0.5*fieldofview.polar
+	};
+	for i in 0..resolution.width {
+		for j in 0..resolution.height {
+			//convert to correct coords
+			fire_point = CartCoord::from(SphereCoord {
+				r:projection_radius,
+				azimuthal:start_ray.azimuthal+i*angle_delta,
+				polar:start_ray.polar+j*angle_delta
+			});
+			//fire ray
+			//record result
+		}
+	}
+}
