65e303060e78e7be38ce59779e5f986d5a282ede
PrestonCazier <prestoncazier@gmail.com>
2021-07-06 10:24:54 -0700

    error fix for cast_to_sphere param change

--- src/primitive/ray.rs
+++ src/primitive/ray.rs
@@ -23,8 +23,8 @@ pub fn cast_to_sphere(
 	let eye_z = eye_coord.z - sphere_coord.z;
 
 	//Do raycast
-	let a = ray_x.powi(2) + ray_y.powi(2) + ray_z.powi(2);
-	let b = 2.0 * eye_x * ray_x + 2.0 * eye_y * ray_y + 2.0 * eye_z * ray_z;
+	let a = ray_direction.x.powi(2) + ray_direction.y.powi(2) + ray_direction.z.powi(2);
+	let b = 2.0 * eye_x * ray_direction.x + 2.0 * eye_y * ray_direction.y + 2.0 * eye_z * ray_direction.z;
 	let c = eye_x.powi(2) + eye_y.powi(2) + eye_z.powi(2) - sphere_radius.powi(2);
 	let result_one = (-1.0 * b + (b.powi(2) - 4.0 * a * c).sqrt()) / 2.0 * a;
 	let result_two = (-1.0 * b - (b.powi(2) - 4.0 * a * c).sqrt()) / 2.0 * a;
